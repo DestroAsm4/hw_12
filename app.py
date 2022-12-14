e7aada20550d59c616cf99c35601c0e55acfd5e2
Asm <destroasm@gmail.com>
2022-12-14 13:11:24 +0300

    finish

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, send_from_directory
 from main.main import main
 from loader.loader import loader
 import os
 
-picfolder = os.path.join('uploads','images')
+picfolder = os.path.join('uploads', 'images')
 
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = picfolder
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 
 # открытие папки upload для внешнего доступа
@@ -21,4 +22,3 @@ def static_dir(path):
 app.register_blueprint(main)
 app.register_blueprint(loader)
 app.run()
-
